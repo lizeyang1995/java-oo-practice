358f3cd0b6943c4cedb69153a9e736b9696f9460
lizeyang1995 <424011527@qq.com>
2020-09-02 10:18:22 +0800

    [lizeyang] feat: add a class named User

---
+++ src/com/twu/User.java
@@ -0,0 +1,9 @@
+public class User {
+    private String userName;
+    private int ticketsNumber;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.ticketsNumber = 10;
+    }
+}
