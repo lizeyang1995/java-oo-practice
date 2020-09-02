806cdbb7dece61e24515995b18c8b2a3f04f6e61
lizeyang1995 <424011527@qq.com>
2020-09-02 16:13:30 +0800

    [lizeyang] feat: add return in method bidding

--- src/com/twu/PriceController.java
+++ src/com/twu/PriceController.java
@@ -10,13 +10,14 @@ public class PriceController {
         allRankingPrices = new ArrayList<>();
     }
 
-    public void bidding(int newRank, int originalRanking, int price) {
+    public boolean bidding(int newRank, int originalRanking, int price) {
         if (allRankingPrices.get(newRank - 1) < price) {
             System.out.println("购买成功！");
             allRankingPrices.set(newRank - 1, price);
             allRankingPrices.set(originalRanking - 1, 0);
-        } else {
-            System.out.println("购买失败！");
+            return true;
         }
+        System.out.println("购买失败！");
+        return false;
     }
 }
