dec3a2368ef8d063906a8841ffe6d41cf53124e6
lizeyang1995 <424011527@qq.com>
2020-09-02 14:54:53 +0800

    [lizeyang] feat: add a new method named action in AdministratorPage

--- src/com/twu/AdministratorPage.java
+++ src/com/twu/AdministratorPage.java
@@ -7,6 +7,37 @@ public class AdministratorPage {
         this.administrator = administrator;
     }
 
+    public void action() {
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            showAdministratorPage();
+            int operationNumber = scanner.nextInt();
+            switch (operationNumber) {
+                case 1: {
+                    administrator.showEvent();
+                    break;
+                }
+                case 2: {
+                    System.out.println("请输入要添加的热搜名称：");
+                    String eventName = scanner.next();
+                    administrator.addEvent(eventName, false);
+                    break;
+                }
+                case 3: {
+                    System.out.println("请输入要添加的超级热搜名称：");
+                    String eventName = scanner.next();
+                    administrator.addEvent(eventName, true);
+                    break;
+                }
+                case 4: {
+                    run = false;
+                    break;
+                }
+            }
+        }
+    }
+
     public void showAdministratorPage() {
         System.out.println("你好，" + administrator.getAdministratorName() + "，你可以：");
         System.out.println("1.查看热搜排行榜");
