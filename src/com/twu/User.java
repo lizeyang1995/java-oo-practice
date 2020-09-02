0af632e2575e499ea22a2fbd8faa0cea80d54421
lizeyang1995 <424011527@qq.com>
2020-09-02 11:09:37 +0800

    [lizeyang] feat: add a method named viewEvents

--- src/com/twu/User.java
+++ src/com/twu/User.java
@@ -1,9 +1,15 @@
 public class User {
     private String userName;
     private int ticketsNumber;
+    private EventController eventController;
 
     public User(String userName) {
         this.userName = userName;
         this.ticketsNumber = 10;
+        this.eventController = new EventController();
+    }
+
+    public void viewEvents() {
+        eventController.showEvent();
     }
 }
