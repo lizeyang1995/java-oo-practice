6b41a86c012dc7a1679c4217ecb279dee39db69c
lizeyang1995 <424011527@qq.com>
2020-09-02 20:29:34 +0800

    [lizeyang] feat: add a new HashMap named eventNewRank

--- src/com/twu/RankController.java
+++ src/com/twu/RankController.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RankController {
     private List<Integer> obtainedRanking;
     private List<Integer> allRankingPrices;
+    private Map<String, Integer> eventNewRank;
 
     public RankController() {
         obtainedRanking = new ArrayList<>();
         allRankingPrices = new ArrayList<>();
+        eventNewRank = new HashMap<>();
     }
 
     public List<Integer> getObtainedRanking() {
@@ -18,12 +22,13 @@ public class RankController {
         return allRankingPrices;
     }
 
-    public void bidding(int newRank, int originalRanking, int price) {
+    public void bidding(int newRank, int originalRanking, String eventName, int price) {
         if (allRankingPrices.get(newRank - 1) < price) {
             System.out.println("竞价成功！");
             allRankingPrices.set(newRank - 1, price);
             allRankingPrices.set(originalRanking - 1, 0);
             obtainedRanking.set(originalRanking - 1, newRank);
+            eventNewRank.put(eventName, newRank);
         } else {
             System.out.println("竞价失败！");
         }
--- src/com/twu/User.java
+++ src/com/twu/User.java
@@ -43,7 +43,7 @@ public class User {
     public void purchaseEvent(String eventName, int newRank, int price) {
         int eventRank = eventController.getEventRank(eventName);
         if (eventRank > -1 && eventRank != newRank) {
-            rankController.bidding(newRank, eventRank, price);
+            rankController.bidding(newRank, eventRank, eventName, price);
         } else {
             System.out.println("购买失败！");
         }
