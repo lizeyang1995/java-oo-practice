6b3d2c3a7a0988be8b90fbf65169e2c57a56730c
lizeyang1995 <424011527@qq.com>
2020-09-02 14:51:30 +0800

    [lizeyang] feat: add a new method named showAdministratorPage in AdministratorPage

--- src/com/twu/AdministratorPage.java
+++ src/com/twu/AdministratorPage.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class AdministratorPage {
     private Administrator administrator;
 
     public AdministratorPage(Administrator administrator) {
         this.administrator = administrator;
+    }
+
+    public void showAdministratorPage() {
+        System.out.println("你好，" + administrator.getAdministratorName() + "，你可以：");
+        System.out.println("1.查看热搜排行榜");
+        System.out.println("2.添加热搜");
+        System.out.println("3.添加超级热搜");
+        System.out.println("4.退出");
     }
 }
