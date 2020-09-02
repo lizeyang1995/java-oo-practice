ed2ceadcdde87adea9c5f9c1140c0c993ca1a5e9
lizeyang1995 <424011527@qq.com>
2020-09-02 16:10:11 +0800

    [lizeyang] feat: add a new method named bidding

--- src/com/twu/PriceController.java
+++ src/com/twu/PriceController.java
@@ -9,4 +9,14 @@ public class PriceController {
         this.eventController = eventController;
         allRankingPrices = new ArrayList<>();
     }
+
+    public void bidding(int newRank, int originalRanking, int price) {
+        if (allRankingPrices.get(newRank - 1) < price) {
+            System.out.println("购买成功！");
+            allRankingPrices.set(newRank - 1, price);
+            allRankingPrices.set(originalRanking - 1, 0);
+        } else {
+            System.out.println("购买失败！");
+        }
+    }
 }
